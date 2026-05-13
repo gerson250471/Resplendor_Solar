fd390f3d1d5b221b6c4f963229a593966a3fe2ac
Gerson Bernardo <gbds25@gmail.com>
2026-05-13 16:55:49 -0300

    Corrigindo Lançamento

--- backend/main.js
+++ backend/main.js
@@ -150,8 +150,8 @@ function salvarVendaNoServidor(venda, parcelas) {
 
     // 2. Grava todas as Parcelas de uma vez respeitando a nova coluna "Pago em"
     parcelas.forEach(p => {
-      // Estrutura: [ID, Cliente, Vencimento, Valor, Pago em (vazio), Status]
-      abaParcelas.appendRow([venda.id, venda.cliente, p.vencimento, p.valor, "", "EM ABERTO"]);
+      // Estrutura: [ID, N° Parcela, Vencimento, Valor, Pago em (vazio), Status]
+      abaParcelas.appendRow([venda.id, venda.parcela, p.vencimento, p.valor, "", "EM ABERTO"]);
     });
 
     // --- BLOCO DE CONTROLE DE VERSÃO ---
